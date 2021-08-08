c9fdc396c6daa68262ba16e2992fed0cdcc28cb0
Moises Miguel <moimigueldev@gmail.com>
2021-08-08 15:21:02 -0500

    created hour, minutes and seconds arms

--- sketch.js
+++ sketch.js
@@ -1,22 +1,79 @@
-let angle = 0;
-
+let arm;
 function setup() {
   createCanvas(400, 400);
-  // put setup code here
   angleMode(DEGREES);
+  arm = new Arm();
 }
 
 function draw() {
   background(50);
   translate(width / 2, height / 2);
 
-  noFill();
-  stroke(255);
-  strokeWeight(2);
-  push();
-  rotate(-90);
-  arc(0, 0, 50, 50, 0, angle);
-  pop();
+  // SECONDS ARM
+  arm.update();
+  arm.showSeconds();
 
-  angle += 1;
+  // Minute ARM
+  arm.showMinutes();
+
+  // HOUR ARM
+  arm.showHours();
+
+  // CENTER DOT
+  stroke(255);
+  strokeWeight(8);
+  point(0, 0);
+}
+
+class Arm {
+  constructor() {
+    this.seconds = this.getSeconds();
+    this.minutes = this.getMinutes();
+    this.hours = this.getHours();
+  }
+
+  update() {
+    this.seconds = this.getSeconds();
+    this.minutes = this.getMinutes();
+    this.hours = this.getHours();
+  }
+
+  showSeconds() {
+    push();
+    strokeWeight(5);
+    rotate(this.seconds);
+    stroke('#E59500');
+    line(0, 0, 0, -height / 3.8);
+    pop();
+  }
+
+  showMinutes() {
+    push();
+    strokeWeight(5);
+    rotate(this.minutes);
+    stroke('#840032');
+    line(0, 0, 0, -height / 4);
+    pop();
+  }
+
+  showHours() {
+    push();
+    strokeWeight(5);
+    rotate(this.hours);
+    stroke('#002642');
+    line(0, 0, 0, -height / 5.5);
+    pop();
+  }
+
+  getMinutes() {
+    return map(new Date().getMinutes(), 0, 60, 0, 360);
+  }
+
+  getHours() {
+    return map(new Date().getHours() - 12, 0, 12, 0, 360);
+  }
+
+  getSeconds() {
+    return map(new Date().getSeconds(), 0, 60, 0, 360);
+  }
 }
